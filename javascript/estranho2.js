cfd39c3b532f0c8360c7a1f6eb4faae1e21e8715
pedro santos <ps3979187@gmail.com>
2021-11-24 03:24:16 -0300

    alteracoes no dr. estranho

--- javascript/estranho2.js
+++ javascript/estranho2.js
@@ -3,10 +3,10 @@ function responda() {
     while (resposta !== 1 || resposta !== 2) {
         resposta = prompt("Digite a resposta: ")
         if (resposta == 1) {
-            window.location.href = "../html/estranho2.html"
+            window.location.href = "../html/gameOverEstranho.html"
             break
         } else if (resposta == 2) {
-            window.location.href = "../html/gameOverEstranho.html"
+            window.location.href = "../html/estranho3.html"
             break
         } else {
             alert("Escolha somente entre as opções abaixo!")
--- javascript/estranho3.js
+++ javascript/estranho3.js
@@ -2,11 +2,11 @@ function responda() {
     var resposta
     while (resposta !== 1 || resposta !== 2) {
         resposta = prompt("Digite a resposta: ")
-        if (resposta == 1) {
-            window.location.href = "../html/estranho2.html"
-            break
-        } else if (resposta == 2) {
+        if (resposta == 2) {
             window.location.href = "../html/gameOverEstranho.html"
+            break
+        } else if (resposta == 1) {
+            window.location.href = "../html/vencedor.html"
             break
         } else {
             alert("Escolha somente entre as opções abaixo!")
