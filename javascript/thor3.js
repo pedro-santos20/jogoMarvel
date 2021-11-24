aa325d61ce63158cd53cac2e7fe766253f0926b8
pedro santos <ps3979187@gmail.com>
2021-11-24 16:18:46 -0300

    alteracoes semi-finais nos arquivos do thor

--- javascript/thor3.js
+++ javascript/thor3.js
@@ -3,10 +3,10 @@ function responda() {
     while (resposta !== 1 || resposta !== 2) {
         resposta = prompt("Digite a resposta: ")
         if (resposta == 2) {
-            window.location.href = "../html/gameOverEstranho.html"
+            window.location.href = "../html/gameOverThor.html"
             break
         } else if (resposta == 1) {
-            window.location.href = "../html/vencedorEstranho.html"
+            window.location.href = "../html/vencedorThor.html"
             break
         } else {
             alert("Escolha somente entre as opções abaixo!")
