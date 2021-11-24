089180454592c16d31fb63d69c148010d3f5c2cb
pedro santos <ps3979187@gmail.com>
2021-11-24 16:07:39 -0300

    fases 1 e 2 do thor completas

--- javascript/thor2.js
+++ javascript/thor2.js
@@ -3,10 +3,10 @@ function responda() {
     while (resposta !== 1 || resposta !== 2) {
         resposta = prompt("Digite a resposta: ")
         if (resposta == 1) {
-            window.location.href = "../html/thor2.html"
+            window.location.href = "../html/gameOverThor.html"
             break
         } else if (resposta == 2) {
-            window.location.href = "../html/gameOverThor.html"
+            window.location.href = "../html/thor3.html"
             break
         } else {
             alert("Escolha somente entre as opções abaixo!")
--- javascript/thor3.js
+++ javascript/thor3.js
@@ -2,11 +2,11 @@ function responda() {
     var resposta
     while (resposta !== 1 || resposta !== 2) {
         resposta = prompt("Digite a resposta: ")
-        if (resposta == 1) {
-            window.location.href = "../html/thor2.html"
+        if (resposta == 2) {
+            window.location.href = "../html/gameOverEstranho.html"
             break
-        } else if (resposta == 2) {
-            window.location.href = "../html/gameOverThor.html"
+        } else if (resposta == 1) {
+            window.location.href = "../html/vencedorEstranho.html"
             break
         } else {
             alert("Escolha somente entre as opções abaixo!")
